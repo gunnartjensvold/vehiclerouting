7ba818190e95b773304780fdb42661c757c5d514
gunnartjensvold <gunnar_97@hotmail.no>
2023-12-16 22:41:01 +0100

    creating structs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-fn main() {
-    let mut x: i32 = 2; 
-    println!("Hello, world!");
-
-    println!("x is {x}");
-    double(&mut x);
-    println!("x is {x}");
+struct Node {
+    id: u16, // Index in distance matrix
+    duration: i32, // Duration required to serve
 }
 
-fn double(x: &mut i32){
-    *x *= 4;
+struct Vehicle {
+    id: u16,
+    nodes: Vec<Node>
+}
+
+struct Graph {
+    distance_matrix : Vec<Vec<f32>>
+}
+
+fn main() {
+
+    let mut solution: Vec<i32> = vec![0,0,1,2,3];
+
+    for node in solution.iter(){
+        println!("{node}");
+    }
+
+    solution[0] = 1;
+    double(&mut solution);
+
+    println!("");
+
+    for node in solution.iter(){
+        println!("{node}");
+    }
+}
+
+fn double(x: &mut Vec<i32>){
+    x[0] = 3;
 }
