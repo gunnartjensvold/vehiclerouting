126a440e4c4de4527915868f38b8b191deb9e267
Gunnar Tjensvold <gunnar_97@hotmail.no>
2023-12-16 14:49:40 +0100

    mut

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+    let mut x: i32 = 2; 
     println!("Hello, world!");
+
+    println!("x is {x}");
+    double(&mut x);
+    println!("x is {x}");
+}
+
+fn double(x: &mut i32){
+    *x *= 4;
 }
